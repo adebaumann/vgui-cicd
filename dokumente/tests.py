4d1232b7645b768e130b4d5b8e9b6e8b8440d6b4
Adrian A. Baumann <ade@adebaumann.com>
2025-10-23 16:35:31 +0200

    Tests - not runnable yet.

--- dokumente/tests.py
+++ dokumente/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from myapp.models import Dokument
 
 # Create your tests here.
+
+class DokumentTestCase (TestCase):
+    def setUp(self):
+        Document.objects.create(name)
